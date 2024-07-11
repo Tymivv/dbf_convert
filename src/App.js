cd071d84768c12cd4a43bd21240e68f4162d0a2c
Tymivv <if210683tvn@gmail.com>
2024-07-11 17:14:57 +0300

    encoding

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef } from "react";
+import React, {useState, useRef, useEffect } from "react";
 import { read, utils, writeFileXLSX } from 'xlsx';
 import { decodeString } from './utils';
 import style from './app.module.css';
@@ -6,27 +6,32 @@ window.Buffer = window.Buffer || require("buffer").Buffer;
 
 export default function App() {
   const [table, setTable] = useState([]);
+  const [table0, setTable0] = useState([]);
   const [loader, setLoader] = useState(false);
   const [error, setError] = useState(null);
   const [tableValid, setTableValid] = useState(false);
+  const [selectedEncoding, setSelectedEncoding] = useState('');
+  const [decodedData, setDecodedData] = useState([]);
   const fileInputRef = useRef(null);
 
-  const decodeObject = (obj) => {
-    const decodedObj = {};
-    for (const key in obj) {
-      if (typeof obj[key] === 'string') {
-        decodedObj[key] = decodeString(obj[key]);
-      } else {
-        decodedObj[key] = obj[key];
-      }
-    }
-    return decodedObj;
-  };
 
-  const handleFixEncoding = () => {
+  useEffect(() => {
+    const decodeObject = (obj) => {
+      const decodedObj = {};
+      for (const key in obj) {
+        if (typeof obj[key] === 'string') {
+          decodedObj[key] = decodeString(obj[key], selectedEncoding);
+        } else {
+          decodedObj[key] = obj[key];
+        }
+      }
+      return decodedObj;
+    };
     const decoded = table.map(item => decodeObject(item));
-    setTable(decoded);
-  };
+    setDecodedData(decoded);
+    setTableValid(true); 
+
+  }, [selectedEncoding]);
 
   const openFileInput = () => {
     fileInputRef.current.click();
@@ -42,7 +47,8 @@ export default function App() {
         const ws= wb.Sheets[wb.SheetNames[0]];
         setTable(utils.sheet_to_json(ws));
         setLoader(false);
-        setTableValid(true); 
+        setTable0(0);
+        console.log(table0);
     } catch (error) {
         setError('Помилка читання файла', error);
       }
@@ -50,7 +56,7 @@ export default function App() {
   ;
 
   const exportFile = async () => {
-    const ws = utils.json_to_sheet(table);
+    const ws = utils.json_to_sheet(decodedData);
     const wb = utils.book_new();
     utils.book_append_sheet(wb, ws, "Data");
     let d = new Date().getTime()
@@ -58,6 +64,7 @@ export default function App() {
     setTableValid(false);
     setTable([])  
   };
+
 
   return (
     <div className={style.container}>
@@ -71,26 +78,43 @@ export default function App() {
             <button 
                 className={style.btn} 
                 onClick={openFileInput}>
-                select table
+                вибрати
             </button>
+            
+        <select 
+        className={style.btn}
+          value={selectedEncoding} 
+          onChange={(e) => setSelectedEncoding(e.target.value)}
+        >
+          <option value="" disabled  >виберіть кодування</option>
+          <option value="utf-8">UTF-8</option>
+          <option value="utf-7">UTF-7</option>
+          <option value="windows-1251">Windows-1251</option>
+          <option value="windows-1252">Windows-1252</option>
+          <option value="windows-1256">Windows-1256</option>
+          <option value="koi8-r">KOI8-R</option>
+          <option value="iso-8859-1">ISO-8859-1</option>
+          <option value="cp866">cp866</option>
+          <option value="cp1251">cp1251</option>
+          <option value="cp1252">cp1252</option>
+          <option value="ibm866">ibm866</option>
+          <option value="0">залишити без змін</option>
+        </select>
+    
             <button 
                 className={style.btn}
-                onClick={handleFixEncoding}>
-                change encoding
-            </button>
-            <button 
-                className={style.btn}
-                disabled={!tableValid} 
+                disabled={!tableValid && selectedEncoding==='' } 
                 onClick={exportFile}>
-                Export XLSX
+                Експорт XLSX
             </button>
           {error && <div>{error}</div>}
-          {loader && <p className={style.firstLine_loading}>loading...</p>}
-          {table.length>0 && 
+          {/* {table.length>0 && <p className={style.firstLine_loading}>таблиця завантажина</p>}   */}
+          {loader && <p className={style.firstLine_loading}>loading...</p>}       
+          {decodedData.length>0 && 
               <div 
               className={style.firstLine}>
                   <h2 className={style.firstLine_title}>First line</h2>
-                  {Object.entries(table[0]).map(([key, value]) => (
+                  {Object.entries(decodedData[0]).map(([key, value]) => (
                   <p className={style.firstLine_item}key={key}>{`${key}: ${value}`}</p>
                   ))}
               </div>}
--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,12 @@
 import iconv from 'iconv-lite';
+import { Buffer } from 'buffer';
 
-export const decodeString = (str) => {
-  const encodings = ['utf-8', 'windows-1251', 'windows-1252', 'iso-8859-1'];
-
-  for (const encoding of encodings) {
-    try {
-      const decoded = iconv.decode(Buffer.from(str, 'binary'), encoding);
-      if (/^[\x00-\x7F]*$/.test(decoded) === false) {
-        return decoded;
-      }
-    } catch (e) {
-      
-    }
+export const decodeString = (str, encoding) => {
+  try {
+    const buffer = Buffer.from(str, 'binary');
+    const decoded = iconv.decode(buffer, encoding);
+    return decoded;
+  } catch (e) {
+    return str; // Якщо не вдалося декодувати, повертаємо оригінальний рядок
   }
-
-  return str; 
 };
